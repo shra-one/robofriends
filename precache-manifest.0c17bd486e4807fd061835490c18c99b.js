b68d5244052302b4b60bbd2b4d9c1b4a7b7ffe23
shrawan <shrawankumarshrestha@gmail.com>
2020-04-03 15:18:49 +0545

    Updates

---
+++ precache-manifest.0c17bd486e4807fd061835490c18c99b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f268cc609871c7d285560cadfe9951d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4a2c609cda28891c384b",
+    "url": "/robofriends/static/css/main.5e2d41d5.chunk.css"
+  },
+  {
+    "revision": "0eaf75d1c1c5bb509301",
+    "url": "/robofriends/static/js/2.8c8608b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.8c8608b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2c609cda28891c384b",
+    "url": "/robofriends/static/js/main.b36b92e3.chunk.js"
+  },
+  {
+    "revision": "a6516d54d7f182e40a49",
+    "url": "/robofriends/static/js/runtime-main.5c125cc2.js"
+  }
+]);
